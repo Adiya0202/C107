95b0e74a54a0f870591ea1af6d5e750fb1e4ca63
Adiya0202 <80259912+Adiya0202@users.noreply.github.com>
2021-10-12 16:53:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function start {
+    navigator.mediaDevices.getUserMedia({
+audio:true
+    });
+    classifier= ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/I8MWq1Xqc/model.json',modelLoaded);
+}
